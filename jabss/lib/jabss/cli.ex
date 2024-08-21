083f03cbd1978ed70e4bed8a0383b3239609d786
tmurray <tmurray@wumpus-cave.net>
2024-08-20 19:38:03 -0500

    Parse args and show a help screen

--- jabss/lib/jabss/cli.ex
+++ jabss/lib/jabss/cli.ex
@@ -1,5 +1,25 @@
 defmodule Jabss.CLI do
-  def main( _args \\ [] ) do
-    IO.puts( "Hello, world" )
+  def main( args \\ [] ) do
+    { subcommand, other_args } = if length( args ) > 0 do
+      { hd( args ), tl( args ) }
+    else
+      { "help", [] }
+    end
+
+    case subcommand do
+      "help" -> help( other_args )
+      _ -> help( other_args )
+    end
+  end
+
+  def help( _args ) do
+    IO.puts(
+      """
+      jabss - Just A Bunch of Shell Scripts CI/CD
+
+      Commands:
+        help - This help screen
+      """
+    )
   end
 end
