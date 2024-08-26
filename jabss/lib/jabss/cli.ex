0e0146c12aedc6dd8f685b2480360285d279bf4f
tmurray <tmurray@wumpus-cave.net>
2024-08-26 15:12:24 -0500

    Template output

--- jabss/lib/jabss/cli.ex
+++ jabss/lib/jabss/cli.ex
@@ -7,6 +7,7 @@ defmodule Jabss.CLI do
     end
 
     case subcommand do
+      "tmpl" -> tmpl( other_args )
       "help" -> help( other_args )
       _ -> help( other_args )
     end
@@ -19,7 +20,16 @@ defmodule Jabss.CLI do
 
       Commands:
         help - This help screen
+        tmpl - Create a template from the configuration
       """
     )
+  end
+
+
+  def tmpl( args ) do
+    tmpl = hd( args )
+    Jabss.conf_file()
+    |> Jabss.exec_tmpl( tmpl )
+    |> IO.puts()
   end
 end
